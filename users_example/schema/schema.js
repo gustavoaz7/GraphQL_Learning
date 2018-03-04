3f57fc54595038c0099a202453043cb537e66068
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2018-03-04 19:51:51 -0300

    Fix circular reference & Add query fragment example

--- users_example/schema/schema.js
+++ users_example/schema/schema.js
@@ -4,21 +4,38 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        .then(res => res.data)
+      }
+    }
+  })
 })
+
+/*  Query Fragment 
+fragment companyDetails on Company {
+  id,
+  name,
+  description
+}
+use on query:  ...comapanyDetails
+*/
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -30,7 +47,7 @@ const UserType = new GraphQLObjectType({
         .then(res => res.data)
       }
     }
-  }
+  })
 })
 
 const RootQuery = new GraphQLObjectType({
@@ -41,6 +58,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id }`)
+        .then(res => res.data)
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: {id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id }`)
         .then(res => res.data)
       }
     }
